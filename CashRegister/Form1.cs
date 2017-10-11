39786fd108d57a79aaa21fd5470bf3ad91e7d32a
Juliaaa <Juliaaa@DESKTOP-HG8OIJ9>
2017-10-11 18:34:12 -0400

    €

--- CashRegister/Form1.cs
+++ CashRegister/Form1.cs
@@ -16,10 +16,10 @@ namespace CashRegister
     public partial class Form1 : Form
     {
         //Globals
-        const double BURGER_PRICE = 2.49;
-        const double FRIES_PRICE = 1.89;
-        const double DRINK_PRICE = 0.99;
-        const double TAX_RATE = 0.13;
+        const double BURGER_PRICE = 2.58;
+        const double FRIES_PRICE = 1.98;
+        const double DRINK_PRICE = 0.98;
+        const double TAX_RATE = 0.19;
 
         int totalBurger, totalFries, totalDrinks;
         
@@ -54,9 +54,9 @@ namespace CashRegister
                 totalCost = totalTax + subTotalCost;
                 
                 //Output Messages
-                subTotalOutput.Text = subTotalCost.ToString("C");
-                TaxOutput.Text = totalTax.ToString("C");
-                totalOutput.Text = totalCost.ToString("C");
+                subTotalOutput.Text = subTotalCost.ToString("0.00" + " €");
+                TaxOutput.Text = totalTax.ToString("0.00") + " €";
+                totalOutput.Text = totalCost.ToString("0.00") + " €";
             }
             catch
             {
@@ -78,7 +78,7 @@ namespace CashRegister
             changeDue = tendered - totalCost;
 
             //Output Messages
-            changeOutput.Text = changeDue.ToString("C");
+            changeOutput.Text = changeDue.ToString("0.00") + " €";
             }
             catch
             {
@@ -130,7 +130,7 @@ namespace CashRegister
 
             fg.FillRectangle(wBrush, 282, 152, 266, 15);
 
-            fg.DrawString("Nummer der Bestellung: 420", bFont, bBrush, 300, 152);
+            fg.DrawString("Bestellnummer: 420", bFont, bBrush, 300, 152);
             Thread.Sleep(400);
 
             fg.FillRectangle(wBrush, 282, 167, 266, 15);
@@ -145,21 +145,21 @@ namespace CashRegister
             
             fg.DrawString("Frühstücksburger", bFont, bBrush, 300, 197);
             fg.DrawString("x " + totalBurger, bFont, bBrush, 430, 197);
-            fg.DrawString(totalBP.ToString("C"), bFont, bBrush, 475, 197);
+            fg.DrawString(totalBP.ToString("0.00") + " €", bFont, bBrush, 475, 197);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 212, 266, 15);
 
             fg.DrawString("Gitterkartoffeln", bFont, bBrush, 300, 212);
             fg.DrawString("x " + totalFries, bFont, bBrush, 430, 212);
-            fg.DrawString(totalFP.ToString("C"), bFont, bBrush, 475, 212);
+            fg.DrawString(totalFP.ToString("0.00") + " €", bFont, bBrush, 475, 212);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 227, 266, 15);
 
             fg.DrawString("Getränke", bFont, bBrush, 300, 227);
             fg.DrawString("x " + totalDrinks, bFont, bBrush, 430, 227);
-            fg.DrawString(totalDP.ToString("C"), bFont, bBrush, 475, 227);
+            fg.DrawString(totalDP.ToString("0.00") + " €", bFont, bBrush, 475, 227);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 242, 266, 15);
@@ -168,19 +168,19 @@ namespace CashRegister
             fg.FillRectangle(wBrush, 282, 257, 266, 15);
 
             fg.DrawString("Netto", bFont, bBrush, 300, 257);
-            fg.DrawString(subTotalCost.ToString("C"), bFont, bBrush, 475, 257);
+            fg.DrawString(subTotalCost.ToString("0.00") + " €", bFont, bBrush, 475, 257);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 272, 266, 15);
 
             fg.DrawString("Steuern", bFont, bBrush, 300, 272);
-            fg.DrawString(totalTax.ToString("C"), bFont, bBrush, 475, 272);
+            fg.DrawString(totalTax.ToString("0.00") + " €", bFont, bBrush, 475, 272);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 287, 266, 15);
 
             fg.DrawString("Brutto", bFont, bBrush, 300, 287);
-            fg.DrawString(totalCost.ToString("C"), bFont, bBrush, 475, 287);
+            fg.DrawString(totalCost.ToString("0.00") + " €", bFont, bBrush, 475, 287);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 302, 266, 15);
@@ -189,13 +189,13 @@ namespace CashRegister
             fg.FillRectangle(wBrush, 282, 317, 266, 15);
 
             fg.DrawString("Bar", bFont, bBrush, 300, 317);
-            fg.DrawString(tendered.ToString("C"), bFont, bBrush, 475, 317);
+            fg.DrawString(tendered.ToString("0.00") + " €", bFont, bBrush, 475, 317);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 332, 266, 15);
 
             fg.DrawString("Wechselgeld", bFont, bBrush, 300, 332);
-            fg.DrawString(changeDue.ToString("C"), bFont, bBrush, 475, 332);
+            fg.DrawString(changeDue.ToString("0.00") + " €", bFont, bBrush, 475, 332);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 347, 266, 15);
@@ -209,12 +209,12 @@ namespace CashRegister
 
             fg.FillRectangle(wBrush, 282, 392, 266, 15);
 
-            fg.DrawString("McDonald's Restaurant", bFont, bBrush, 325, 392);
+            fg.DrawString("McDonald's Restaurant", bFont, bBrush, 340, 392);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 407, 266, 15);
 
-            fg.DrawString("Itterstraße 99 / 40589 Düsseldorf", bFont, bBrush, 290, 407);
+            fg.DrawString("Itterstraße 99 D-40589 Düsseldorf", bFont, bBrush, 290, 407);
             Thread.Sleep(time);
             fg.FillRectangle(wBrush, 282, 422, 266, 15);
 
@@ -223,7 +223,7 @@ namespace CashRegister
 
             fg.FillRectangle(wBrush, 282, 437, 266, 15);
 
-            fg.DrawString("Telefonnummer: 0211/3983890", bFont, bBrush, 305, 437);
+            fg.DrawString("Telefonnummer: 0211/3983890", bFont, bBrush, 315, 437);
             Thread.Sleep(time);
 
             fg.FillRectangle(wBrush, 282, 452, 266, 15);
